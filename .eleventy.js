e0df6f29fe1a938e52066cbb221329b27f36ba24
Jason Garber <jason@sixtwothree.org>
2023-01-06 10:33:14 -0500

    Simplify passthrough copy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,10 @@
-module.exports = eleventy => {
-  eleventy.addPassthroughCopy('./src/fonts');
-  eleventy.addPassthroughCopy('./src/images');
-  eleventy.addPassthroughCopy('./src/javascripts');
-  eleventy.addPassthroughCopy('./src/public');
-  eleventy.addPassthroughCopy('./src/stylesheets');
+module.exports = eleventyConfig => {
+  eleventyConfig
+    .addPassthroughCopy('./src/fonts')
+    .addPassthroughCopy('./src/images')
+    .addPassthroughCopy('./src/javascripts')
+    .addPassthroughCopy('./src/public')
+    .addPassthroughCopy('./src/stylesheets');
 
   return {
     dir: {
