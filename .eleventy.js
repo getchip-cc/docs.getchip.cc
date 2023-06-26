d073e7e38a2f0da3485f1c7ef6b02bd395b2d1b6
Jason Garber <jason@sixtwothree.org>
2023-06-26 15:57:17 -0400

    Add fonts and images

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(require('./lib/plugins/sass.js'));
 
+  eleventyConfig
+    .addPassthroughCopy('./src/fonts')
+    .addPassthroughCopy('./src/icon-16.png')
+    .addPassthroughCopy('./src/images');
+
   return {
     dir: {
       input: './src',
