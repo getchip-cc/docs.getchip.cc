eec28158e9041a74fdb3594b24a1cdb4d8b8d51c
Jason Garber <jason@sixtwothree.org>
2023-06-26 22:49:04 -0400

    Add and configure markdown-it and plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(require('./lib/plugins/sass.js'));
 
+  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown.js'));
+
   eleventyConfig
     .addPassthroughCopy('./src/fonts')
     .addPassthroughCopy('./src/icon-16.png')
@@ -9,6 +11,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: './src',
+      layouts: './_layouts',
       output: './public'
     }
   };
