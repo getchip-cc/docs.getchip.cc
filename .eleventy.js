a509b5ffd100cae17d770503e0371bcbc8ae36c0
Jason Garber <jason@sixtwothree.org>
2023-06-27 16:01:28 -0400

    Remove ./ from layouts config

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: './src',
-      layouts: './_layouts',
+      layouts: '_layouts',
       output: './public'
     }
   };
