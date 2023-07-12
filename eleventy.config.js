71d52a977298445df0a6f6be8954c7366ed3ded7
Jason Garber <jason@sixtwothree.org>
2023-07-11 22:12:27 -0400

    Tidy up Eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,15 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(require('./lib/plugins/sass.js'));
-
-  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown.js'));
-
+  // Passthrough File Copy
   eleventyConfig
     .addPassthroughCopy('./src/*.{ico,txt}')
     .addPassthroughCopy('./src/fonts')
     .addPassthroughCopy('./src/images');
+
+  // Libraries
+  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown.js'));
+
+  // Plugins
+  eleventyConfig.addPlugin(require('./lib/plugins/sass.js'));
 
   return {
     dir: {
